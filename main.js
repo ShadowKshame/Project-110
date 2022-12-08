cc80e0e6c85a650ec4ff9b02451aabeb398f9fcc
ShadowKshame <104213448+ShadowKshame@users.noreply.github.com>
2022-12-07 20:54:35 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ Webcam.attach('#camera');
 function take_snapshot()
 {
     Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML + '<img id="captured_image" src"'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
@@ -33,4 +33,71 @@ function speak()
     speak_data_2 = "And the second prediction is" + prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_hand_name").innerHTML = results[0].label;
+        document.getElementById("result_hand_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label == "Nice")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "Thumbs up")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "Peace")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#9996;";
+        }
+        if(results[0].label == "up")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#9757;";
+        }
+        if(results[0].label == "fist")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#9994;";
+        }
+        if(results[0].label == "L")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#128070;";
+        }
+        if(results[1].label == "Nice")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#128076;";
+        }
+        if(results[1].label == "Thumbs up")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#128077;";
+        }
+        if(results[1].label == "Peace")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#9996;";
+        }
+        if(results[1].label == "up")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#9757;";
+        }
+        if(results[1].label == "fist")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#9994;";
+        }
+        if(results[1].label == "L")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#128070;";
+        }
+    }
 }
